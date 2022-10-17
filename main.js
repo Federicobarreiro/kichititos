2ddafb8487667e6ff08657a4c7fc4a92c370bac0
Federico Barreiro <barreirofede777@gmail.com>
2022-10-17 19:47:25 -0300

    cambios en article e implementación de mapa

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Menú desplegable
 const btnMenu = document.querySelector("#btnMenu");
 const menu = document.querySelector("#menu");
 
@@ -5,3 +6,17 @@ btnMenu.addEventListener("click", function() {
   menu.classList.toggle("mostrar");
 });
 
+// Implementación de mapa
+function initMap() {
+  let coord = {lat: -35.173193,
+  lng: -58.2256466}
+  let map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 15,
+      center: coord
+  });
+
+  let marker = new google.maps.Marker({
+    position: coord,
+    map: map
+})
+}
